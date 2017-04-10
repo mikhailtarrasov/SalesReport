916c659c87de74ae30141f67e72b2ecd7780aa88
Михаил <mikhailtarrasov@gmail.com>
2017-04-10 16:37:55 +0500

    Появилась возможность выбрать отчётный период

---
+++ Тестовое задание/Тестовое задание/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+using Тестовое_задание.Models;
+
+namespace Тестовое_задание.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NorthwindNETEntities Db = new NorthwindNETEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ReportingPeriod()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReportingPeriod(ReportingPeriodViewModel period)
+        {
+            if (period.StartDate != null && period.EndDate != null)
+            {
+                return RedirectToAction("Report", "Home", new
+                {
+                    startDate = period.StartDate, 
+                    endDate = period.EndDate
+                });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Report(DateTime? startDate, DateTime? endDate)
+        {
+            if (startDate.HasValue && endDate.HasValue)
+            {
+                ViewBag.startDate = startDate.Value.ToString("dd.MM.yyyy");
+                ViewBag.endDate = endDate.Value.ToString("dd.MM.yyyy");
+                return View();
+            }
+            return RedirectToAction("ReportingPeriod", "Home");
+        }
+    }
+}
---
+++ Тестовое задание/Тестовое задание/Models/ReportingPeriodViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Тестовое_задание.Models
+{
+    public class ReportingPeriodViewModel
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
